30152262b94b4283d5e77ccbc85b59e840e26d8a
renshunwei <rsw1992@163.com>
2017-08-16 18:45:40 +0800

    Initial commit

---
+++ src/main/java/com/p2peye/irecyclerview/OnLoadMoreListener.java
@@ -0,0 +1,12 @@
+package com.p2peye.irecyclerview;
+
+import android.view.View;
+
+/**
+ * Created by aspsine on 16/3/13.
+ */
+public interface OnLoadMoreListener {
+
+    void onLoadMore(View loadMoreView);
+
+}
